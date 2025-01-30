1d164a87dfe876d2b3f5cad62300b6add00f528a
henricaioo <12201000103@muz.ifsuldeminas.edu.br>
2025-01-30 16:56:12 -0300

    views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import *
+from django.views import View
+from django.contrib import messages
 
-# Create your views here.
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
+    def post(self, request):
+        pass
+
+class ItensView(View):
+    def get(self, request, *args, **kwargs):
+        itens = Item.objects.all()
+        return render(request, 'itens.html', {'itens' : itens})
+    def post(self, request):
+        pass
+
+class AtributosView(View):
+    def get(self, request, *args, **kwargs):
+        atributos = Atributo.objects.all()
+        return render(request, 'atributos.html', {'atributos' : atributos})
+    def post(self, request):
+        pass
+
+class PericiasView(View):
+    def get(self, request, *args, **kwargs):
+        pericias = Pericia.objects.all()
+        return render(request, 'pericias.html', {'pericias' : pericias})
+    def post(self, request):
+        pass
+
+class PessoasView(View):
+    def get(self, request, *args, **kwargs):
+        pessoas = Pessoa.objects.all()
+        return render(request, 'pessoas.html', {'pessoas' : pessoas})
+    def post(self, request):
+        pass
+
+class CampanhasView(View):
+    def get(self, request, *args, **kwargs):
+        campanhas = Campanha.objects.all()
+        return render(request, 'campanhas.html', {'campanhas' : campanhas})
+    def post(self, request):
+        pass
--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,10 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('itens/', ItensView.as_view(), name='itens'),
+    path('atributos/', AtributosView.as_view(), name='atributos'),
+    path('pericias/', PericiasView.as_view(), name='pericias'),
+    path('pessoas/', PessoasView.as_view(), name='pessoas'),
+    path('campanhas/', CampanhasView.as_view(), name='campanhas'),
 ]
